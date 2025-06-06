c8015d5050abd7b44d81564203d541994dd10dcb
Vtchoo <vittor-limma@hotmail.com>
2025-06-05 22:20:25 -0300

    create game interface

---
+++ src/game/interface.ts
@@ -0,0 +1,30 @@
+
+
+export interface GameWord {
+    word: string
+    lemma?: string
+    distance?: number
+    error?: string // Error message if the word was invalid
+}
+
+export interface Guess {
+    word: string
+    lemma?: string
+    distance?: number
+    addedBy: string
+    error?: string // Error message if the guess was invalid
+}
+
+export interface IGame {
+    gameId: number
+    // players: string[]
+    // guesses: GameWord[]
+    finished: boolean
+
+    addPlayer(playerId: string): void
+    removePlayer(playerId: string): void
+    addGuess(playerId: string, guess: GameWord): void
+    tryWord(playerId: string, word: string): Promise<GameWord>
+    getClosestGuesses(playerId: string, count?: number): GameWord[]
+    startGame(): void
+}
